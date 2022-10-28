a3afe18cb37a84fa7e0f9d6c6fe9727dc8be9d89
Shion-Aoki <ec2-user@ip-172-31-2-28.ec2.internal>
2022-10-28 19:28:37 +0900

    Third commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,10 +10,10 @@
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
-            <div class='post'>
-                <h2 class='title'>{{ $post->title }}</h2>
-                <p class='body'>{{ $post->body }}</p>
-            </div>
+                <div class='post'>
+                    <a herf="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
             @endforeach
         </div>
         <div class='paginate'>{{ $posts->links() }}</div>
